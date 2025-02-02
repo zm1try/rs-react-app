52a4763d458a7a0f2c7ac8bf60c7b1cd26c6d942
Dzmitry_Yarash <dzmitry_yarash@epam.com>
2025-02-02 21:19:55 +0300

    fix: fixed props type

--- src/SearchComponent.tsx
+++ src/SearchComponent.tsx
@@ -3,8 +3,8 @@ import { SearchComponentState } from './SearchComponentState.interface.ts';
 import { Search } from './Search.tsx';
 import { Results } from './Results.tsx';
 
-export class SearchComponent extends Component<{}, SearchComponentState> {
-  constructor(props: {}) {
+export class SearchComponent extends Component<null, SearchComponentState> {
+  constructor(props: null) {
     super(props);
     this.state = {
       items: [],
